3e688c680095bc4235f8480d24d4a7ba8df72c6f
smrv21-15 <smrv21-15@c16x22.iescierva.net>
2024-02-05 19:28:12 +0100

    Practica4_1

--- Principal.java
+++ Principal.java
@@ -1,6 +1,14 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class Principal{
 	public static void main(String[] arg){
 		System.out.println("Hola mundo");
-		System.out.println("Prueba");
+		fecha();
+	}
+	public static void fecha(){
+		Date objDate = new Date();
+		String strDateFormat = "HH:mm:ss dd-MM-yyyy";
+  		SimpleDateFormat objSDF = new SimpleDateFormat(strDateFormat);
+  		System.out.println(objSDF.format(objDate));
 	}
 }
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Entorno de pruebas para probar Git
 ## Ejemplo1.java
 >Este fichero es un ejemplo que muestra un hola mundo y crea una variable con un nombre que luego muestra en un mensaje
+## Practica4_1.java
+>Este fichero muestra un Hola Mundo y la hora y fecha actual
